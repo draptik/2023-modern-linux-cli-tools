a94051f4f0285447c7cc311b7b3d40f450c5aa2b
Patrick Drechsler <patrick.drechsler@mathema.de>
2023-06-23 17:50:55 +0200

    🍱 add link and qr code for socrates day

--- presentation/scripts/create-qr-code-for-website.sh
+++ presentation/scripts/create-qr-code-for-website.sh
@@ -4,4 +4,5 @@
 # Also adopt URL (last parameter)...
 
 #qrencode -s 6 -l H -o "../public/images/slides-mddevdays.png" "https://draptik.github.io/2023-05-magdeburger-devdays-modern-linux-cli-tools"
-qrencode -s 6 -l H -o "../public/images/slides-cloudland.png" "https://draptik.github.io/2023-06-cloudland-modern-linux-cli-tools"
+#qrencode -s 6 -l H -o "../public/images/slides-cloudland.png" "https://draptik.github.io/2023-06-cloudland-modern-linux-cli-tools"
+qrencode -s 6 -l H -o "../public/images/slides-socrates-day-franken.png" "https://draptik.github.io/2023-06-socrates-day-franken-modern-linux-cli-tools"
