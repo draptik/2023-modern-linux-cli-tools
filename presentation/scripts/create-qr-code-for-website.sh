6265e81b23984027bf62eb92ad2c45f33f130986
Patrick Drechsler <patrick.drechsler@mathema.de>
2023-09-19 20:15:40 +0200

    ✨ Setup for socrates at: Update README, final slide with QR code and correct link to presentation

--- presentation/scripts/create-qr-code-for-website.sh
+++ presentation/scripts/create-qr-code-for-website.sh
@@ -5,4 +5,5 @@
 
 #qrencode -s 6 -l H -o "../public/images/slides-mddevdays.png" "https://draptik.github.io/2023-05-magdeburger-devdays-modern-linux-cli-tools"
 #qrencode -s 6 -l H -o "../public/images/slides-cloudland.png" "https://draptik.github.io/2023-06-cloudland-modern-linux-cli-tools"
-qrencode -s 6 -l H -o "../public/images/slides-socrates-day-franken.png" "https://draptik.github.io/2023-06-socrates-day-franken-modern-linux-cli-tools"
+#qrencode -s 6 -l H -o "../public/images/slides-socrates-day-franken.png" "https://draptik.github.io/2023-06-socrates-day-franken-modern-linux-cli-tools"
+qrencode -s 6 -l H -o "../public/images/slides-socrates-day-at.png" "https://draptik.github.io/2023-09-socrates-day-at-modern-linux-cli-tools"
